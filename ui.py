0705fab8104b8373b8d0abf91b539cc4cb0616a1
Lumantis <131993718+Lumantis@users.noreply.github.com>
2023-06-13 14:03:40 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,31 +1,32 @@
 from discord import ui, ButtonStyle
 
 class PauseResumeButton(ui.Button['PauseResumeButton']):
-    def __init__(self):
+    def __init__(self, player):
         super().__init__(style=ButtonStyle.secondary, label="Pause/Resume", custom_id="pause_resume")
+        self.player = player
 
     async def callback(self, interaction):
-        player = players.get(interaction.guild.id)
-        if player:
-            if player.voice_client.is_playing():
+        if self.player:
+            if self.player.voice_client.is_playing():
                 await interaction.response.send_message('Mise en pause.', ephemeral=True)
-                await player.pause_or_resume('pause')
-            elif player.voice_client.is_paused():
+                await self.player.pause_or_resume('pause')
+            elif self.player.voice_client.is_paused():
                 await interaction.response.send_message('Reprise.', ephemeral=True)
-                await player.pause_or_resume('resume')
+                await self.player.pause_or_resume('resume')
+
 
 class SkipButton(ui.Button['SkipButton']):
-    def __init__(self):
+    def __init__(self, player):
         super().__init__(style=ButtonStyle.primary, label="Passer", custom_id="skip")
+        self.player = player
 
     async def callback(self, interaction):
-        player = players.get(interaction.guild.id)
-        if player:
-            await player.stop_or_skip('skip')
+        if self.player:
+            await self.player.stop_or_skip('skip')
         await interaction.response.send_message('Skip.', ephemeral=True)
 
 class MusicButtonsView(ui.View):
-    def __init__(self):
+    def __init__(self, player):
         super().__init__()
-        self.add_item(PauseResumeButton())
-        self.add_item(SkipButton())
+        self.add_item(PauseResumeButton(player))
+        self.add_item(SkipButton(player))
