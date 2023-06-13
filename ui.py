fda6201513cb8872bfc541d9d8fb0c9367c8fbfd
Lumantis <131993718+Lumantis@users.noreply.github.com>
2023-06-13 14:42:21 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,16 +2,16 @@ from discord import ui, ButtonStyle
 
 class PauseResumeButton(ui.Button['PauseResumeButton']):
     def __init__(self, player):
-        super().__init__(style=ButtonStyle.secondary, label="Pause/Resume", custom_id="pause_resume")
+        super().__init__(style=ButtonStyle.secondary, label="Pause/Lecture", custom_id="pause_resume")
         self.player = player
 
     async def callback(self, interaction):
         if self.player and self.player.voice_client:
             if self.player.voice_client.is_playing():
-                await interaction.response.send_message('Mise en pause.', ephemeral=True)
+                await interaction.response.send_message('Mise en pause de la musique en cours.', ephemeral=True)
                 await self.player.pause_or_resume('pause')
             elif self.player.voice_client.is_paused():
-                await interaction.response.send_message('Reprise.', ephemeral=True)
+                await interaction.response.send_message('Reprise de la musique en cours.', ephemeral=True)
                 await self.player.pause_or_resume('resume')
 
 
@@ -23,7 +23,7 @@ class SkipButton(ui.Button['SkipButton']):
     async def callback(self, interaction):
         if self.player and self.player.voice_client:
             await self.player.stop_or_skip('skip')
-        await interaction.response.send_message('Skip.', ephemeral=True)
+        await interaction.response.send_message('La musique a bien été passée.', ephemeral=True)
 
 class MusicButtonsView(ui.View):
     def __init__(self, player):
