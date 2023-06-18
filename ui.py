4d13496b761aae629ab837deedb93e82c86116c8
Lumantis <131993718+Lumantis@users.noreply.github.com>
2023-06-18 13:59:34 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,29 +1,43 @@
-from discord import ui, ButtonStyle
+from discord import ui, ButtonStyle, Interaction, Button
+from discord.errors import HTTPException
 
 class PauseResumeButton(ui.Button['PauseResumeButton']):
     def __init__(self, player):
-        super().__init__(style=ButtonStyle.secondary, label="Pause/Lecture", custom_id="pause_resume")
+        super().__init__(style=ButtonStyle.secondary, emoji="⏸️", label="Pause/Lecture", custom_id="pause_resume")
         self.player = player
 
-    async def callback(self, interaction):
+    async def callback(self, interaction: Interaction):
         if self.player and self.player.voice_client:
             if self.player.voice_client.is_playing():
-                await interaction.response.send_message('Mise en pause de la musique en cours.', ephemeral=True)
-                await self.player.pause_or_resume('pause')
+                try:
+                    await interaction.response.send_message('Mise en pause de la musique en cours.', ephemeral=True)
+                    await self.player.pause_or_resume('pause')
+                except HTTPException:
+                    await interaction.response.send_message('Impossible de mettre la musique en pause.', ephemeral=True)
             elif self.player.voice_client.is_paused():
-                await interaction.response.send_message('Reprise de la musique en cours.', ephemeral=True)
-                await self.player.pause_or_resume('resume')
+                try:
+                    await interaction.response.send_message('Reprise de la musique en cours.', ephemeral=True)
+                    await self.player.pause_or_resume('resume')
+                except HTTPException:
+                    await interaction.response.send_message('Impossible de reprendre la musique.', ephemeral=True)
+        else:
+            await interaction.response.send_message('Je ne suis pas connecté à un canal vocal.', ephemeral=True)
 
 
 class SkipButton(ui.Button['SkipButton']):
     def __init__(self, player):
-        super().__init__(style=ButtonStyle.primary, label="Passer", custom_id="skip")
+        super().__init__(style=ButtonStyle.primary, emoji="⏭️", label="Passer", custom_id="skip")
         self.player = player
 
-    async def callback(self, interaction):
+    async def callback(self, interaction: Interaction):
         if self.player and self.player.voice_client:
-            await self.player.stop_or_skip('skip')
-        await interaction.response.send_message('La musique a bien été passée.', ephemeral=True)
+            try:
+                await self.player.stop_or_skip('skip')
+                await interaction.response.send_message('La musique a bien été passée.', ephemeral=True)
+            except HTTPException:
+                await interaction.response.send_message('Impossible de passer la musique.', ephemeral=True)
+        else:
+            await interaction.response.send_message('Je ne suis pas connecté à un canal vocal.', ephemeral=True)
 
 class MusicButtonsView(ui.View):
     def __init__(self, player):
